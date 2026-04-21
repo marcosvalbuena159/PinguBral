0304063da0ec058f82610fb3e7332b08bcd3b148
marcosvalbuena159 <marcos.valbuena560@gmail.com>
2026-04-21 18:13:47 -0500

    fourth commit

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://uqtoprfpfgxejdvsmlvo.supabase.co'
-const supabaseKey = 'sb_publishable_AyjHbCHVb0OSu5_6JSVIug_QB7P8QkH'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxdG9wcmZwZmd4ZWpkdnNtbHZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1MTQwNjMsImV4cCI6MjA5MTA5MDA2M30.R8z5OTezM1TtivcXoVWRtZ2EM_u7HYn8BzybFFE84M4'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
